9df6d7a1a50a48627c8363518cdc1fe5df9554e6
Fabio Rodrigo de Souza <dev.fabio.rs@gmail.com>
2024-09-01 11:31:11 -0300

    feat(data) add data context

---
+++ DigitalBank.DataContext.MySql/DigitalBankContext.cs
@@ -0,0 +1,46 @@
+﻿using DigitalBank.DataContext.MySql.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+using DigitalBank.EntityModels;
+
+namespace DigitalBank.DataContext.MySql;
+
+public class DigitalBankContext : DbContext
+{
+    public DigitalBankContext()
+    {
+
+    }
+
+    public DigitalBankContext(DbContextOptions<DigitalBankContext> options)
+    : base(options)
+    {
+    }
+    public DbSet<Client> Clients { get; set; }
+
+    public DbSet<Account> Accounts { get; set; }
+
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString =
+            "Data Source=mysql01-dev-fabio-rs.mysql.database.azure.com;" +
+            "Initial Catalog=digital_bank;" +
+            "Username=digital_bank;" +
+            "Password=cacce5QuacOw|Orjyep@;" +
+            "SslMode=Required";
+
+        var serverVersion = new MySqlServerVersion(new Version(8, 0));
+
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AccountEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new ClientEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new TransactionEntityTypeConfiguration());
+    }
+}
---
+++ DigitalBank.DataContext.MySql/DigitalBankContextExtensions.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore; // UserMysql
+using Microsoft.Extensions.DependencyInjection; // IServiceCollection
+
+namespace DigitalBank.DataContext.MySql;
+
+public static class NorthwindContextExtensions
+{
+    /// <summary>
+    /// Adds NorthwindContext to the specified IServiceCollection. Uses the SqlServer database provider.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="connectionString">Set to override the default.</param>
+    /// <returns>An IServiceCollection that can be used to add more services.</returns>
+    public static IServiceCollection AddDigitalBankContext(
+      this IServiceCollection services,
+      string connectionString = 
+            "Data Source=mysql01-dev-fabio-rs.mysql.database.azure.com;" +
+            "Initial Catalog=digital_bank;" +
+            "Username=digital_bank;" +
+            "Password=cacce5QuacOw|Orjyep@;" +
+            "SslMode=Required")
+    {
+        var serverVersion = new MySqlServerVersion(new Version(8, 0));
+
+        services.AddDbContext<DigitalBankContext>(options =>
+        {
+            options.UseMySql(connectionString, serverVersion);
+
+            options.LogTo(Console.WriteLine,
+          new[] { Microsoft.EntityFrameworkCore
+          .Diagnostics.RelationalEventId.CommandExecuting });
+        });
+
+        return services;
+    }
+}
